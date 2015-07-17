2912f4760225ca389d62442e8a265ac70751abe8
AlecSKlein <alecsklein@gmail.com>
2015-07-16 23:06:04 -0500

    Toying with idea of rounded fragment borders and button coloring

--- app/src/main/java/com/giftly/giftly/ForgotRegisterFragment.java
+++
@@ -1,9 +0,0 @@
-package com.giftly.giftly;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by Alec on 7/16/2015.
- */
-public class ForgotRegisterFragment extends Fragment {
-}
--- app/src/main/java/com/giftly/giftly/LogoFragment.java
+++ app/src/main/java/com/giftly/giftly/LogoFragment.java
@@ -1,9 +1,19 @@
 package com.giftly.giftly;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Alec on 7/15/2015.
  */
 public class LogoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.logo_fragment, container, false);
+        return v;
+    }
 }
